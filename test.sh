bdd4260b8d3aa69d7778f458043dd128981b66eb
Tommi Junttila <Tommi.Junttila@aalto.fi>
2021-08-18 13:41:37 +0300

    Update Z3 to 4.8.12, use pip to install it, and update grade-python to 3.7-3.5-3.4

--- test.sh
+++ test.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-echo " -------- Building test image -------------"
+echo " --------- Building test image --------------"
 docker build -t apluslms/grading-z3:test .
 
-echo " -------- Executing test image ------------"
+echo " --------- Printing Z3 version --------------"
+if docker run --rm -it -v `pwd`:/submission apluslms/grading-z3:test z3 --version; then
+    echo " -- OK!"
+else
+    echo " !!! ERROR !!! "
+fi
+
+echo " --- Solving a simple problem via Python ---"
 if docker run --rm -it -v `pwd`:/submission apluslms/grading-z3:test python3 test.py; then
     echo " -- OK!"
 else
